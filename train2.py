eb59592e20167d10a5fef599005cfc62eb8912e7
vtsen <zhenhumit@gmail.com>
2017-12-09 18:45:52 -0500

    Rough version with MMD domain transfer added

--- train2.py
+++ train2.py
@@ -21,40 +21,45 @@ def train_model(s_train, sd_train, s_dev, s_test,
 	q_parameters = filter(lambda p: p.requires_grad, q_model.parameters())
 	d_parameters = filter(lambda p: p.requires_grad, d_model.parameters())
 
-        optimizer = None
-        q_opt = torch.optim.Adam(q_parameters,lr=args.lr)
-        d_opt = torch.optim.Adam(d_parameters,lr=-args.lr_d)
+	optimizer = None
+	q_opt = torch.optim.Adam(q_parameters,lr=args.lr)
+	d_opt = torch.optim.Adam(d_parameters,lr=-args.lr_d)
 
 	scores = []
 	for epoch in range(1, args.epochs+1):
 		print("-------------\nEpoch {}:\n".format(epoch))
+		if args.full_eval and not args.use_mmd:
+			print('\n\n\n####\n\n')
 
 		# train
 		loss1,loss2 = run_epoch(s_train, sd_train, t_train, 
                                         q_model, d_model, 
                                         q_opt, d_opt, args)
 
+		print(args.use_mmd)
+
 		print('Train loss: {}, {}'.format(loss1, loss2))
-		#torch.save(q_model, args.save_path)	
+		torch.save(q_model, './mod' + str(epoch) + '.pkl')#args.save_path)
 
-		#print('\nEvaluating on source dev')
-		#evaluate.q_evaluate(q_model, s_dev, args)
+#		print('\nEvaluating on source dev')
+#		evaluate.q_evaluate(q_model, s_dev, args)
 
-		#print('Evaluating on source test')
-		#evaluate.q_evaluate(q_model, s_test, args)
+#		print('Evaluating on source test')
+#		evaluate.q_evaluate(q_model, s_test, args)
 
-                print('\nEvaluating on target dev')
+		print('\nEvaluating on target dev')
 		evaluate.q_evaluate(q_model, t_dev, args)
 
 		print('Evaluating on target test')
 		evaluate.q_evaluate(q_model, t_test, args)
 
-                if args.full_eval:
-                    print('Evaluating domain classifier on dev')
-                    evaluate.d_evaluate(q_model, d_model, s_dev, t_dev)
+		if args.full_eval and not args.use_mmd:
+			print('Evaluating domain classifier on dev')
+			evaluate.d_evaluate(q_model, d_model, s_dev, t_dev)
 
-                    print('Evaluating domain classifier on test')
-                    evaluate.d_evaluate(q_model, d_model, s_test, t_test)
+			print('Evaluating domain classifier on test')
+			evaluate.d_evaluate(q_model, d_model, s_test, t_test)
+
 
 def get_pos_neg(idx_to_cand, idx_to_vec, ids, titles):
 	pos_batch = []
@@ -78,6 +83,7 @@ def get_pos_neg(idx_to_cand, idx_to_vec, ids, titles):
 	return torch.LongTensor(new_titles),\
 		   torch.LongTensor(pos_batch),\
 		   torch.LongTensor(neg_batch)
+
 
 def mmloss(q, p_plus, ps, args):
 	#q: (bs,Co), p_plus: (bs, Co), ps: (neg_samples, bs, Co)	
@@ -104,6 +110,31 @@ def mmloss(q, p_plus, ps, args):
 	score,_ = torch.max(scores,0)
 	return torch.mean(score)
 
+
+def calc_mmd_loss(bottleneck, y):
+	'''
+	Given bottleneck layer representations for sample points from S and T,
+	with y as the domain indicator, calculate the Maximum Mean Discrepancy
+	value of the two distributions
+	
+	Indicator y: 1 for source, 0 for target
+	'''
+	y = y.type(torch.ByteTensor)
+	
+	repr_len = bottleneck.size()[1]
+	# (bs, repr_len)
+	phi_s = torch.t(torch.t(bottleneck).masked_select(y).view(repr_len,-1))
+	phi_t = torch.t(torch.t(bottleneck).masked_select(y^1).view(repr_len,-1))
+	
+	phi_s = torch.mean(phi_s, dim=0, keepdim=True)	# (1, repr_len)
+	phi_t = torch.mean(phi_t, dim=0, keepdim=True)
+	
+	#TODO: parameterize p-norm
+	pdist = nn.PairwiseDistance(p=2)
+	mmd = pdist(phi_s, phi_t)
+	return mmd
+	
+	
 def run_epoch(s_data, sd_data, t_data, q_model, d_model, q_opt, d_opt, args):
 	# load random batches
 	source_query_data = torch.utils.data.DataLoader(
@@ -112,27 +143,27 @@ def run_epoch(s_data, sd_data, t_data, q_model, d_model, q_opt, d_opt, args):
 		shuffle=True,
 		num_workers=4,
 		drop_last=True)
-        domain_bs = t_data.__len__()/(s_data.__len__()/float(args.batch_size))
-        domain_bs = int(domain_bs) + 1
-        source_domain_data = torch.utils.data.DataLoader(
+	domain_bs = t_data.__len__()/(s_data.__len__()/float(args.batch_size))
+	domain_bs = int(domain_bs) + 1
+	source_domain_data = torch.utils.data.DataLoader(
 		sd_data,
 		batch_size=domain_bs,
 		shuffle=True,
 		num_workers=4,
 		drop_last=True)
-        target_domain_data = torch.utils.data.DataLoader(
+	target_domain_data = torch.utils.data.DataLoader(
 		t_data,
 		batch_size=domain_bs,
 		shuffle=True,
 		num_workers=4,
 		drop_last=True)
-        source_domain_data = iter(source_domain_data)
-        target_domain_data = iter(target_domain_data)
+	source_domain_data = iter(source_domain_data)
+	target_domain_data = iter(target_domain_data)
 
 	losses1 = []
 	losses2 = []
 
-        criterion = torch.nn.NLLLoss()
+	criterion = torch.nn.NLLLoss()
 
 	# train on each batch
 	for s_batch in tqdm(source_query_data):
@@ -153,10 +184,16 @@ def run_epoch(s_data, sd_data, t_data, q_model, d_model, q_opt, d_opt, args):
             q_opt.zero_grad()
         
             # run the batch through the model
-            q = q_model(q)
-            p_plus = q_model(p_plus)
-            ps = ps.contiguous().view(-1,args.max_title)
-            ps = q_model(ps)
+            if args.use_mmd:
+	            q, _ = q_model(q)
+	            p_plus, _ = q_model(p_plus)
+	            ps = ps.contiguous().view(-1,args.max_title)
+	            ps, _ = q_model(ps)
+            else:
+	            q = q_model(q)
+	            p_plus = q_model(p_plus)
+	            ps = ps.contiguous().view(-1,args.max_title)
+	            ps = q_model(ps)
             
             if args.model == 'cnn':
                     ps = ps.contiguous().view(args.neg_samples+1,-1, 
@@ -168,9 +205,7 @@ def run_epoch(s_data, sd_data, t_data, q_model, d_model, q_opt, d_opt, args):
             # update model
             loss1 = mmloss(q, p_plus, ps, args)			
             losses1.append(loss1.cpu().data[0])
-            #loss1.backward()
-            #q_opt.step()
-
+           
             # check if we should continue 
             if not args.full_eval: 
                 loss1.backward()
@@ -191,19 +226,30 @@ def run_epoch(s_data, sd_data, t_data, q_model, d_model, q_opt, d_opt, args):
             y = autograd.Variable(torch.cat([y1,y0],0))
             
             q_opt.zero_grad()
-            d_opt.zero_grad()
+            if not args.use_mmd:
+				d_opt.zero_grad()
           
-            out = q_model(x) 
-            out = d_model(out)
-       
-            loss2 = criterion(out,y)			
-            loss = loss1 - args.lambd * loss2
+            if args.use_mmd:
+				_, bottleneck = q_model(x)
+				loss2 = calc_mmd_loss(bottleneck, y)
+				loss = loss1 + args.lambda_mmd * (loss2 ** 2)
+				
+            else:
+	            out = q_model(x) 
+	            out = d_model(out)
+	       
+	            loss2 = criterion(out,y)			
+	            loss = loss1 - args.lambd * loss2
+				
             loss.backward()
-            #loss2.backward(retain_graph=True)
-            d_opt.step()
+			
+            if args.use_mmd:
+				d_opt.step()
+				
             q_opt.step()
 
             losses2.append(loss2.cpu().data[0])
+
 
 	# Calculate epoch level scores
 	avg_loss1 = np.mean(losses1)
